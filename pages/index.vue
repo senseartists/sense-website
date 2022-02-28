7fbe3fa64a2dce28f7caab40f157b2e611802312
GB M <gbm.workshop@gmail.com>
2022-02-28 11:27:02 +0100

    push refs online

---
+++ pages/index.vue
@@ -0,0 +1,190 @@
+<template>
+  <div>
+    <section class="_intro">
+    <div class="intro-block">
+      <p>
+        <span class="svg">
+          Paris
+          <img class="paris" src="~/assets/images/svg/circled_1.svg" alt="">
+        </span> 
+        based artist and label services 
+        solution created <span class="inline-image"><img src="~/assets/images/img-1.jpg" alt=""></span>   to <span class="svg">
+          empower
+          <img class="empower" src="~/assets/images/svg/underline_3.svg" alt="">
+          </span> talents. 
+
+         <span class="inline-image"><img src="~/assets/images/img-5.jpg" alt=""></span> Deeply committed to independence 
+        and <span class="svg">
+          creativity
+          <img class="creativity" src="~/assets/images/svg/underline_1.svg" alt="">
+          </span>.  <span class="inline-image"><img src="~/assets/images/img-8.jpg" alt=""></span> Serving music + artists 
+        interests.
+
+      </p>
+    </div>
+  </section>  
+
+  <section class="_workwith">
+    <h2>we work with</h2>
+    <img src="~/assets/images/svg/smiley.svg" alt="">
+    <div class="row">
+      
+      <div class="column col-3">
+        <div class="img-block _1x1" style="background-color: #FFDAFF">
+          <img src="~/assets/images/grid/alphapup.jpg" alt="">
+          <span class="name">Alpha <br>Pup</span>
+        </div>
+      </div>
+      <div class="column col-3">
+        <div class="img-block _1x1" style="background-color: #B9DAFF">
+          <img src="~/assets/images/grid/menace.jpg" alt="">
+          <span class="name">Menace</span>
+        </div>
+      </div>
+      <div class="column col-3">
+        <div class="img-block _1x1" style="background-color: #FFDAAF">
+          <img src="~/assets/images/grid/horarecords.png" alt="">
+          <span class="name">Hora <br> records</span>
+        </div>
+      </div>
+      <div class="column col-3">
+        <div class="img-block _1x1" style="background-color: #93DAAF">
+          <img src="~/assets/images/grid/toshio.jpg" alt="">
+          <span class="name">Toshio <br> Matsuura</span>
+        </div>
+      </div>
+      <div class="column col-3">
+        <div class="img-block _1x1" style="background-color: #FFDAFF">
+          <img src="~/assets/images/grid/fufu.jpg" alt="">
+          <span class="name">Fufu</span>
+        </div>
+      </div>
+      <div class="column col-3">
+        <div class="img-block _1x1" style="background-color: #B9DAFF">
+          <img src="~/assets/images/grid/jazzysports.jpg" alt="">
+          <span class="name">Jazzy <br> Sport</span>
+        </div>
+      </div>
+      <div class="column col-3">
+        <div class="img-block _1x1" style="background-color: #FFDAAF">
+          <img src="~/assets/images/grid/martasanchez.jpg" alt="">
+          <span class="name">Marta <br> Sanchez</span>
+        </div>
+      </div>
+      <div class="column col-3">
+        <div class="img-block _1x1" style="background-color: #93DAAF">
+          <img src="~/assets/images/grid/jazzycouscous.jpg" alt="">
+          <span class="name">Jazzy <br> Couscous</span>
+        </div>
+      </div>
+      <div class="column col-3">
+        <div class="img-block _1x1" style="background-color: #FFDAFF">
+          <img src="~/assets/images/grid/hugolx.jpg" alt="">
+          <span class="name">Hugo lX</span>
+        </div>
+      </div>
+      <div class="column col-3">
+        <div class="img-block _1x1" style="background-color: #B9DAFF">
+          <img src="~/assets/images/grid/bluestaeb.jpg" alt="">
+          <span class="name">Bluestaeb</span>
+        </div>
+      </div>
+      <div class="column col-3">
+        <div class="img-block _1x1" style="background-color: #FFDAAF">
+          <img src="~/assets/images/grid/enzogarniel.jpg" alt="">
+          <span class="name">Enzo <br> Garniel</span>
+        </div>
+      </div>
+      <div class="column col-3">
+        <div class="img-block _1x1" style="background-color: #93DAAF">
+          <img src="~/assets/images/grid/manymore.jpg" alt="">
+          <span class="name">& many <br> more</span>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="_moveus">
+    <div class="container">
+      <div class="row">
+        <div class="column col-6">
+          <span class="fix"></span>
+          <iframe src="https://open.spotify.com/embed/album/1DFixLWuPkv3KT3TnV35m3" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        </div>
+        <div class="column col-6">
+          <img class="latest" src="~/assets/images/svg/latest_release.svg" alt="">
+          <img class="glimps" src="~/assets/images/svg/glimps.svg" alt="">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="_emailus">
+    <div class="intro-block">
+      <p>
+
+       If you're <span class="svg">
+          an artist, 
+          <img class="artist" src="~/assets/images/svg/circled_3.svg" alt="">
+         </span><span class="inline-image"><img src="~/assets/images/img-9.jpg" alt=""></span> a band, a
+      <span class="inline-image"><img src="~/assets/images/img-11.jpg" alt=""></span> company, or if you just want to say
+      <span class="inline-image"><img src="~/assets/images/img-13.jpg" alt=""></span> hello, <span class="svg">
+        feel free
+         <img class="feelfree" src="~/assets/images/svg/underline_2.svg" alt="">
+        </span> :
+      </p>
+
+      <a href="mailto:contact@senseartists.com" target="_blank">e-mail us</a>
+
+      <img src="~/assets/images/svg/spam.svg" alt="" class="spam">
+    </div>
+  </section>
+
+  <footer>
+    <div class="container">
+
+        <span class="footer-line">
+          <img src="~/assets/images/svg/footer_line.svg" alt="">
+        </span>
+
+        <span class="logo-footer">
+          <img src="~/assets/images/svg/logo_footer.svg" alt="">
+        </span>
+
+        <nav class="links">
+          <a href="https://senseartists.disco.ac/lib/6196" target="_blank">Sync</a>
+          <a href="" target="_blank">Newsletter</a>
+        </nav>
+        <nav class="social">
+          <a href="https://www.instagram.com/senseartists/" target="_blank">
+            <img src="~/assets/images/svg/instagram.svg" alt="">
+          </a>
+          <a href="https://discord.com/invite/5cREszMKhh" target="_blank">
+              <img src="~/assets/images/svg/discord.svg" alt="">
+          </a>
+          <a href="" target="_blank">
+              <img src="~/assets/images/svg/twitter.svg" alt="">
+          </a>
+        </nav>
+    
+        
+
+        <span class="copyright">
+          SENSE, all rights reserved ©2022
+        </span>
+    </div>
+      
+  </footer>
+
+
+
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage'
+})
+</script>
