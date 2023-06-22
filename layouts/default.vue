7f63dcc3aa02c2486f9cdc635ff1bb6028bb36b5
senseartist <contact@senseartists.com>
2023-06-22 15:36:00 +0200

    Mise a jour

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,24 +1,40 @@
 <template>
-  <div v-bind:class="$route.name">
-    <header>
+    <div :class="$route.name">
+      <header>
         <div class="container">
-            <div class="row">
-                <div class="column col-12">
-                    <NuxtLink class="logo" to="/">
-                        <img src="~/assets/images/svg/logo.svg" width="466" alt="SENSE" />
-                        <span>music release club</span>
-                    </NuxtLink>
-                    <a href="https://docs.google.com/forms/d/e/1FAIpQLSc833ZxiyQkrIhwhCApd8hUKXuK60NLWcMWnwrJSUI6r9q5WA/viewform" class="email" target="_blank" title="join">join</a>
-                </div>
-                
+          <div class="row">
+            <div class="column col-12" style="z-index: 1;">
+              <NuxtLink v-if="!showPinkScreen" class="logo" to="/">
+                <img src="~/assets/images/svg/logo.svg" width="466" alt="SENSE" />
+                <span>music release club</span>
+              </NuxtLink>
+              <a v-if="!showPinkScreen" href="https://docs.google.com/forms/d/e/1FAIpQLSc833ZxiyQkrIhwhCApd8hUKXuK60NLWcMWnwrJSUI6r9q5WA/viewform" class="email" target="_blank" title="join">join</a>
             </div>
+          </div>
         </div>
-    </header>
-    <Nuxt />
-  </div>
+      </header>
+      <div class="pink-screen" :class="{ 'hide': showContent, 'show': showPinkScreen }">
+        <!-- Contenu de la div pink-screen -->
+      </div>
+      <Nuxt />
+    </div>
+  </template>
   
-</template>
-
-<script>
-   
-</script>
+  <script>
+    export default {
+      data() {
+        return {
+          showContent: false,
+          showPinkScreen: true
+        };
+      },
+      mounted() {
+        // Simule l'animation de disparition de la div pink-screen
+        setTimeout(() => {
+          this.showContent = true;
+          this.showPinkScreen = false;
+        }, 800); 
+      }
+    }
+  </script>
+  
